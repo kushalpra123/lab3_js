f7b808015475b82109c2343e2864a26952db7644
kushalpra123 <kushalpra49@gmail.com>
2025-11-16 01:34:12 -0500

    made class

---
+++ script.js
@@ -0,0 +1,10 @@
+function ToyCar(name, brand, color, year, price, material, features, imageURL) {
+    this.name = name;
+    this.brand = brand;
+    this.color = color;
+    this.year = year;
+    this.price = price;
+    this.material = material;
+    this.features = features;
+    this.imageURL = imageURL;
+}
